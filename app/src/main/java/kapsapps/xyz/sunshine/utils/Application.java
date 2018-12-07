e308321c4e37d98e7f53e1605de57b96ad3930c4
Kapil Yamakanmardi <kapsmardi@gmail.com>
2018-12-07 12:05:41 +0530

    added morning and night themes

--- app/src/main/java/kapsapps/xyz/sunshine/utils/Application.java
+++ app/src/main/java/kapsapps/xyz/sunshine/utils/Application.java
@@ -1,5 +1,9 @@
 package kapsapps.xyz.sunshine.utils;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import kapsapps.xyz.sunshine.R;
 import timber.log.Timber;
 
 public class Application extends android.app.Application {
@@ -9,6 +13,17 @@ public class Application extends android.app.Application {
         super.onCreate();
 
         Timber.plant(new Timber.DebugTree());
+
+        Date date = new Date();
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+
+        if(hour > 18 || hour < 6) {
+            getApplicationContext().setTheme(R.style.night_theme);
+        }
         
     }
 }
